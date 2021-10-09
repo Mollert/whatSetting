f0c4c8f1632d0b598c5658732171b066515fe2d2
David <blueskies@carolina.rr.com>
2021-10-09 19:17:08 -0400

    add/remove class

--- changeImg.js
+++ changeImg.js
@@ -1,47 +1,22 @@
 
-const shrinkImgSingle = (select) => {
-	document.getElementById(select).style.transform = "scale(1)";	
-	document.getElementById(select).style.transition = "transform 0.4s ease-out";
-	document.getElementById(select).style.borderColor = "black";
-	document.getElementById(select).style.borderWidth = "0.1rem";		
-}
-
-const shrinkImgAll = (all) => {
-	for (let i = 0 ; i < all.length ; i++) {
-		if (document.getElementById(all[i].id).style.borderColor === "firebrick") {
-			shrinkImgSingle(all[i].id);			
-		}
-	}
-}
-
-const expandImg = (tap) => {
-	document.getElementById(tap).style.transform = "scale(18)";
-	document.getElementById(tap).style.transition = "transform 1s ease";
-	document.getElementById(tap).style.borderWidth = "0.001rem";
-	document.getElementById(tap).style.borderColor = "firebrick";
-}
-
-
-let entries = document.getElementsByClassName("theImage");
+let entries = document.getElementsByClassName("smallImage");
 
 for (let i = 0 ; i < entries.length ; i++) {
 	entries[i].addEventListener("mouseover", (event) => {
 		let chosen = event.target.id;
-		expandImg(chosen);
+		document.getElementById(chosen).classList.add("largeImage");
 
 		document.getElementById(chosen).addEventListener("mouseout", () => {
-			shrinkImgAll(entries);
+			document.getElementById(chosen).classList.remove("largeImage");
 		});
 	});
 
 	entries[i].addEventListener("touchstart", (event) => {
 		let match = event.target.id;
+		document.getElementById(match).classList.add("largeImage");
 
-		if(document.getElementById(match).style.borderColor === "firebrick") {
-			shrinkImgSingle(match);
-		} else {
-			shrinkImgAll(entries);	
-			expandImg(match);			
-		}
+		document.getElementById(match).addEventListener("touchend", () => {
+			document.getElementById(match).classList.remove("largeImage");
+		});
 	});
 }
